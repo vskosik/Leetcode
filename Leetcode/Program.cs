a2fd82399cecab51fc01c6d6c49d3bc6de9ff009
vskosikok <vskosikok@gmail.com>
2023-07-26 16:14:50 -0500

    ValidParantheses

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,37 +1,41 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Leetcode
 {
-    internal class Program
+    internal abstract class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             //Console.WriteLine(ConvertTime("02:30", "04:35"));
-            Console.WriteLine(CoinChange(new[] { 186, 419, 83, 408 }, 6249));
-
+            //Console.WriteLine(CoinChange(new[] { 186, 419, 83, 408 }, 6249));
+            Console.WriteLine(ValidParentheses("]"));
         }
 
-        public static int ConvertTime(string current, string correct)
+        private static bool ValidParentheses(string s)
         {
-            var currentMinutes = int.Parse(current[..2]) * 60 + int.Parse(current[3..]);
-            var correctMinutes = int.Parse(correct[..2]) * 60 + int.Parse(correct[3..]);
+            var stack = new Stack<char>();
+            var brackets = new Dictionary<char, char> { { '(', ')' }, { '{', '}' }, { '[', ']' } };
 
-            var diff = correctMinutes - currentMinutes;
-            var operators = new[] { 60, 15, 5, 1 };
-            var count = 0;
-
-            foreach (var op in operators)
+            foreach (var symbol in s)
             {
-                count += diff / op;
-                diff %= op;
+                if (brackets.ContainsKey(symbol))
+                {
+                    stack.Push(symbol);
+                }
+                else if (brackets.ContainsValue(symbol))
+                {
+                    if (stack.Count == 0 || brackets[stack.Pop()] != symbol)
+                    {
+                        return false;
+                    }
+                }
             }
 
-            return count;
+            return stack.Count == 0;
         }
 
-        public static int CoinChange(int[] coins, int amount)
+        private static int CoinChange(int[] coins, int amount)
         {
             var count = 0;
             Array.Sort(coins);
@@ -47,6 +51,24 @@ namespace Leetcode
             if (amount != 0)
             {
                 return -1;
+            }
+
+            return count;
+        }
+
+        private static int ConvertTime(string current, string correct)
+        {
+            var currentMinutes = int.Parse(current[..2]) * 60 + int.Parse(current[3..]);
+            var correctMinutes = int.Parse(correct[..2]) * 60 + int.Parse(correct[3..]);
+
+            var diff = correctMinutes - currentMinutes;
+            var operators = new[] { 60, 15, 5, 1 };
+            var count = 0;
+
+            foreach (var op in operators)
+            {
+                count += diff / op;
+                diff %= op;
             }
 
             return count;
